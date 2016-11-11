a0f392b9a7cd30ca11e22d5ef4aa6c9384f9d3af
Filip Biljecki <fbiljecki@gmail.com>
2016-11-11 14:34:56 +0100

    Prettifying the daily plot (seaborn)

--- dailyplot.py
+++ dailyplot.py
@@ -41,13 +41,13 @@ settings.append({'Name' : 'Surface A',
 })
 
 settings.append({'Name' : 'Surface B',
-'Tilt' : 90.0,
+'Tilt' : 40.0,
 'Azimuth' : 90.0,
 })
 
 #-- Which days
 #-- month, day format
-epochs = [[3, 1], [6, 21]]
+epochs = [[3, 1], [7, 19]]
 
 #-- Sampling interval
 interval = 5
@@ -106,13 +106,20 @@ for epoch in epochs:
                 res[setting['Name']][e].append([dt_, irrValue, horr_irrValue])
 
 import scipy
+import os
+#-- Fix LaTeX for macOS 10.12
+os.environ['PATH'] = os.environ['PATH'] + ':/Library/TeX/texbin'
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
+import seaborn as sns
+#-- Plotting properties
+sns.set(style="white", font='serif', rc={'axes.facecolor': '#FFFFFF', 'grid.linestyle': '', 'axes.grid' : False, 'font.family': ['serif'], 'legend.frameon': True})
+colors = sns.color_palette()
 
-
-fig = plt.figure(1)
+# fig = plt.figure(1)
+fig, ax1 = plt.subplots(figsize=(8, 4))
 timestamp = []
 total_ir = []
 total_ir_h = []
@@ -122,7 +129,7 @@ for v in res['Surface A']['0301']:
         total_ir.append(v[1])
         total_ir_h.append(v[2])
 
-A_1 = plt.plot(timestamp, total_ir, 'r--')
+A_1 = plt.plot(timestamp, total_ir, color=colors[0], linestyle='--', marker='o', markevery=slice(60, 170, 15))
 
 timestamp = []
 total_ir = []
@@ -133,40 +140,49 @@ for v in res['Surface B']['0301']:
         total_ir.append(v[1])
         total_ir_h.append(v[2])
 
-B_1 = plt.plot(timestamp, total_ir, 'b--')
-H_1 = plt.plot(timestamp, total_ir_h, 'k--')
+B_1 = plt.plot(timestamp, total_ir, color=colors[1], linestyle='--', marker='v', markevery=slice(60, 170, 15))
+# H_1 = plt.plot(timestamp, total_ir_h, color=colors[2], linestyle='--')
 
 
 timestamp = []
 total_ir = []
 total_ir_h = []
 
-for v in res['Surface A']['0621']:
+for v in res['Surface A']['0719']:
         timestamp.append(v[0])
         total_ir.append(v[1])
         total_ir_h.append(v[2])
 
-A_2 = plt.plot(timestamp, total_ir, 'r-')
+A_2 = plt.plot(timestamp, total_ir, color=colors[0], linestyle='-', marker='o', markevery=slice(30, 185, 15))
 
 timestamp = []
 total_ir = []
 total_ir_h = []
 
-for v in res['Surface B']['0621']:
+for v in res['Surface B']['0719']:
         timestamp.append(v[0])
         total_ir.append(v[1])
         total_ir_h.append(v[2])
 
-B_2 = plt.plot(timestamp, total_ir, 'b-')
-H_2 = plt.plot(timestamp, total_ir_h, 'k-')
+B_2 = plt.plot(timestamp, total_ir, color=colors[1], linestyle='-', marker='v', markevery=slice(30, 185, 15))
+# H_2 = plt.plot(timestamp, total_ir_h, color=colors[2], linestyle='-')
 
 ddd = scipy.zeros(len(total_ir_h))
 #plt.fill_between(timestamp, total_ir_h, where=total_ir_h>=ddd, interpolate=True, color='k')
 
-plt.title('Clear-sky global solar irradiance for Delft, the Netherlands', size=15)
+import matplotlib.dates as md
+xfmt = md.DateFormatter('%H:%M')
+ax1.xaxis.set_major_formatter(xfmt)
+
+sns.despine(left=False, bottom=False)
+# ax1.tick_params(axis='x', which='major', bottom='on')
+# ax1.set_xlim([734869.16, 734869.84])
+
+# plt.title('Clear-sky global solar irradiance for Delft, the Netherlands', size=15)
 plt.xlabel('Time (UTC)', size=14)
-#plt.ylabel(u'Total solar irradiation (W/m²)', size=14)#$^{2}$
-plt.ylabel(r'Global solar irradiance (W/m$^{2}$)', size=14)#
-plt.legend(['A on 1 Mar', 'B on 1 Mar', 'H on 1 Mar', 'A on 21 Jun', 'B on 21 Jun', 'H on 21 Jun']), #[A_1, B_1, H_1], 
-plt.savefig('dailyplot.pdf')
-plt.show()
+#plt.ylabel(u'Total solar irradiation (W/m²)', size=14)
+plt.ylabel(r'Global solar irradiance (W/m$^{2}$)', size=14)
+plt.legend(['A on 1 Mar', 'B on 1 Mar', 'A on 19 Jul', 'B on 19 Jul'], loc='upper center', bbox_to_anchor=(0.5, 1.15), fancybox=1, shadow=0, ncol=4, numpoints=1, prop={'size':12})
+plt.savefig('dailyplot.png', bbox_inches='tight', dpi=300)
+plt.savefig('dailyplot.pdf', bbox_inches='tight')
+# plt.show()
